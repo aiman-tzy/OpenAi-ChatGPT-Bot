9102d2489c147bbc01e3c58ff2987d94818cb70c
Lynxaiman <aimanmk89@gmail.com>
2023-08-12 07:42:03 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -28,7 +28,7 @@ global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴
 > Ai Bot Password Get From Watching This Video 
 */
 global.AI_PASSWORD = 'king-nima-gpt-bot'
-global.SESSION_ID = ''
+global.SESSION_ID = 'elisa_md@;;;wENkkaiZ#0qGAEzIce6IohOeF4eBnt02YnbLbxjR9yy8uLhRFNwg'
 
 /*
 🎲 𝗢𝗧𝗛𝗘𝗥 𝗦𝗘𝗧𝗜𝗡𝗚𝗦
